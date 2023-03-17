3455b7e246aa7e2604568270133fb4ca4f584a1b
Jackson Meires <lordjackson@gmail.com>
2023-03-17 15:18:42 -0300

    upload do projeto

---
+++ view/UsuarioForm.php
@@ -0,0 +1,26 @@
+<?php 
+include "../controller/UsuarioController.php";
+
+if(!empty($_POST)){
+    $usuario = new UsuarioController();
+    $usuario->salvar($_POST);
+}
+
+?>
+
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+        <form action="UsuarioForm.php" method="POST">
+            <label>Nome</label><br>
+            <input type="text" name="nome"/><br>
+            <label>Telefone</label><br>
+            <input type="text" name="telefone"/><br>
+
+            <input type="submit" value="Salvar"/>
+        </form>
+
+	</body>
+</html>
