683ac00b914efdbe43205e80c8a0b4343cd483a7
Jackson Meires <lordjackson@gmail.com>
2023-03-29 14:41:15 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html>
   <head>
     <title>PHP Test</title>
@@ -6,13 +5,4 @@
   <body>
     <a href="./view/login.php">Logar </a><br>
 	</body>
-=======
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <a href="./view/UsuarioList.php">Listar de Usuários </a>
-	</body>
->>>>>>> 25dcbd872d2ea79cf4bea6ef8c421d39f32e814e
 </html>
